7202c901ccbf42a9e17c436e0cd092fad3504b59
Hyakusetufutou <Hyakusetufutou358@gmail.com>
2024-06-09 16:38:00 +0900

    APIを用いて天気予報を取得し画面表示する処理を実装

--- IOSTraining/Controller/ViewController.swift
+++ IOSTraining/Controller/ViewController.swift
@@ -9,12 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var reloadButton: UIButton!
     
+    var weatherManager = WeatherManager()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        weatherManager.delegate = self
     }
 
+    @IBAction func reloadButtonPressed(_ sender: UIButton) {
+        weatherManager.fetchWeatherCondition()
+    }
+    
+}
 
+extension ViewController: WeatherManagerDelegate {
+    func weatherImageDidUpdate(weatherModel: WeatherModel) {
+        self.weatherImage.image = UIImage(named: weatherModel.type)
+        weatherImage.tintColor = weatherModel.color
+    }
 }
 
